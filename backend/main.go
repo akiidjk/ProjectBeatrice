a940d5075f9c720068043e3d55c5850789dcc82c
Retr0 <dragonfaiver@gmail.com>
2024-07-10 18:51:39 +0200

    Refactor the function and file structure

--- backend/main.go
+++ backend/main.go
@@ -1,102 +1,38 @@
 package main
 
 import (
-	"backend/config"
-	"backend/utils"
-	"bufio"
-	"bytes"
-	"log"
-	"net/http"
+	"backend/logger"
+	"backend/ollama"
+	"backend/routes"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	WarningLogger *log.Logger
-	InfoLogger    *log.Logger
-	ErrorLogger   *log.Logger
-)
-
-func init() {
-	InfoLogger, WarningLogger, ErrorLogger = utils.Init()
-}
-
 // https://github.com/ollama/ollama/blob/main/docs/api.md
 
-func get_status(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status": "online!!!",
-	})
-}
-
-func check_api() int {
-	resp, err := http.Get(config.URL_API + "api/ps")
-	if err != nil {
-		ErrorLogger.Println(err)
-		return 0
-	}
-	return resp.StatusCode
-}
-
-func create_model() {
-	InfoLogger.Println("Creating model...")
-
-	var model = utils.ReadModelfile()
-
-	userData := []byte(`{"name": "beatrice","modelfile": "` + model + `"}`)
-
-	request, error := http.NewRequest("POST", config.URL_API+"api/create", bytes.NewBuffer(userData))
-	request.Header.Set("Content-Type", "application/json; charset=utf-8")
-
-	if error != nil {
-		ErrorLogger.Println(error)
-		utils.Check(error)
-	}
-
-	client := &http.Client{}
-	response, error := client.Do(request)
-
-	if error != nil {
-		ErrorLogger.Println(error)
-		utils.Check(error)
-	}
-
-	reader := bufio.NewReader(response.Body)
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err != nil {
-			ErrorLogger.Println(err)
-			break
-		}
-
-		result := utils.DecodeJson(line)
-		if result != nil {
-			InfoLogger.Println(result["status"])
-		}
-	}
-
-	defer response.Body.Close()
-}
-
 func main() {
+	logger.SetLevel(logger.DebugLevel)
+
 	r := gin.Default()
 
-	InfoLogger.Println("Starting...")
+	logger.Info("Starting...")
 
-	if check_api() != 200 {
-		ErrorLogger.Println("The ollama API is not running or is not reachable .")
+	if ollama.CheckApi() != 200 {
+		logger.Error("The ollama API is not running or is not reachable .")
 		os.Exit(1)
 	}
 
-	create_model()
+	ollama.CreateModel()
 
-	InfoLogger.Println("Model created...")
+	logger.Success("Model created...")
 
-	InfoLogger.Println("Started...")
+	logger.Success("Started...")
 
 	// Define the route
-	r.GET("/", get_status)
+	r.GET("/", routes.GetStatus)
+	r.GET("/version", routes.GetVersion)
+	r.POST("/message", routes.CreateMessage)
 
 	r.Run("127.0.0.1:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
