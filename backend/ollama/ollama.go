a63522fc2db1b55471075a8371b373f4c2a5d4fa
Retr0 <dragonfaiver@gmail.com>
2024-07-14 22:45:04 +0200

    Now the response of model is read in stream

--- backend/ollama/ollama.go
+++ backend/ollama/ollama.go
@@ -7,11 +7,17 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
+var messages []string
+
 func CheckApi() int {
-	resp, error := http.Get(config.URL_API + "api/ps")
+	resp, error := http.Get(config.API_URL + "api/ps")
 	if error != nil {
 		logger.Error(error.Error())
 		return 0
@@ -26,7 +32,7 @@ func CreateModel() {
 
 	userData := []byte(`{"name": "beatrice","modelfile": "` + model + `"}`)
 
-	request, error := http.NewRequest("POST", config.URL_API+"api/create", bytes.NewBuffer(userData))
+	request, error := http.NewRequest("POST", config.API_URL+"api/create", bytes.NewBuffer(userData))
 	request.Header.Set("Content-Type", "application/json; charset=utf-8")
 
 	if error != nil {
@@ -42,6 +48,7 @@ func CreateModel() {
 		utils.Check(error)
 	}
 
+	//Read stream
 	reader := bufio.NewReader(response.Body)
 	for {
 		line, error := reader.ReadBytes('\n')
@@ -53,29 +60,113 @@ func CreateModel() {
 		result := utils.DecodeJson(line)
 		if result != nil {
 			logger.Info(result["status"].(string))
+			if result["total"] != nil {
+				utils.PrintProgressBar(int(result["completed"].(float64)), int(result["total"].(float64)), "Pulling model", "Model downloaded", 50, "=")
+			}
 		}
 	}
 
 	defer response.Body.Close()
 }
 
-func SendMessage(message string) int {
+func SendMessage(c *gin.Context, user_message string) int {
 	logger.Info("Sending message...")
 
+	logger.Debug("Message: " + user_message)
+
+	user_input := []byte(`{"role": "user","content": "` + user_message + `"}`)
+	messages = append(messages, string(user_input))
+
 	data := []byte(`{
-  "model": "beatrice",
-  "messages": [
-    {
-      "role": "user",
-      "content": "` + message + `"
-    }
-  ],
-	"stream": true
+	  "model": "beatrice",
+	  "messages": [` + strings.Join(messages, ",") + `],
+		"stream": true,
+		"json": true
 }`)
 
 	logger.Debug(string(data))
 
-	request, error := http.NewRequest("POST", config.URL_API+"api/chat", bytes.NewBuffer(data))
+	request, err := http.NewRequest("POST", config.API_URL+"api/chat", bytes.NewBuffer(data))
+	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	if err != nil {
+		logger.Error(err.Error())
+		utils.Check(err)
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+
+	if err != nil {
+		logger.Error(err.Error())
+		utils.Check(err)
+	}
+
+	reader := bufio.NewReader(response.Body)
+	var model_message string = ""
+	logger.Info("Streaming data")
+
+	type StreamMessage struct {
+		Index   int
+		Content string
+	}
+
+	chanStream := make(chan StreamMessage, 10)
+
+	go func() {
+		c.Stream(func(w io.Writer) bool {
+			if msg, ok := <-chanStream; ok {
+				c.SSEvent("message", gin.H{
+					"index":   msg.Index,
+					"content": msg.Content,
+				})
+				return true
+			}
+			return false
+		})
+	}()
+
+	index := 0
+
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			logger.Error(err.Error())
+			break
+		}
+		result := utils.DecodeJson(line)
+
+		if result != nil {
+			message, ok := result["message"].(map[string]interface{})
+			if ok {
+				content, ok := message["content"].(string)
+				if ok {
+					fmt.Print(content)
+					model_message += content
+
+					chanStream <- StreamMessage{Index: index, Content: content}
+					index++
+				}
+			}
+		}
+	}
+
+	close(chanStream)
+
+	model_output := []byte(`{"role": "assistant","content": "` + strings.ReplaceAll(model_message, "\n", "") + `"}`)
+	messages = append(messages, string(model_output))
+
+	return 0
+}
+
+func ShowInfoModel(model string, verbose string) (string, int) {
+	logger.Info("Getting info model...")
+
+	data := []byte(`{"name": "` + model + `","verbose": ` + verbose + `}`)
+
+	logger.Debug("Data: " + string(data))
+
+	request, error := http.NewRequest("POST", config.API_URL+"api/show", bytes.NewBuffer(data))
 	request.Header.Set("Content-Type", "application/json; charset=utf-8")
 
 	if error != nil {
@@ -91,28 +182,9 @@ func SendMessage(message string) int {
 		utils.Check(error)
 	}
 
-	reader := bufio.NewReader(response.Body)
-	for {
-		line, error := reader.ReadBytes('\n')
-		if error != nil {
-			logger.Error(error.Error())
-			break
-		}
+	logger.Info("Model info")
+	bodyBytes, _ := io.ReadAll(response.Body)
+	bodyString := string(bodyBytes)
 
-		result := utils.DecodeJson(line)
-		if result != nil {
-			message, ok := result["message"].(map[string]interface{})
-			if ok {
-				content, ok := message["content"].(string)
-				if ok {
-					// logger.Info(content)
-					fmt.Print(content)
-				}
-			}
-		}
-	}
-
-	defer response.Body.Close()
-
-	return response.StatusCode
+	return bodyString, response.StatusCode
 }
