0089fe1dc7e9489888e7691aac3842199f92f4b0
Retr0 <dragonfaiver@gmail.com>
2024-07-14 22:45:32 +0200

    Add new routes for the info model and now the message is passed by post

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,8 +1,10 @@
 package routes
 
 import (
+	"backend/logger"
 	"backend/ollama"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,13 +21,32 @@ func GetVersion(c *gin.Context) {
 	})
 }
 
+type Message struct {
+	Content string
+}
+
 func CreateMessage(c *gin.Context) {
-	return_code := ollama.SendMessage("Oggi cosa hai fatto?")
+	var message Message
+	c.BindJSON(&message)
+	logger.Debug("Message received: " + message.Content)
+	ollama.SendMessage(c, message.Content)
+}
+
+func ShowModel(c *gin.Context) {
+	model := c.Param("model")
+	verbose := strings.ToLower(c.DefaultQuery("verbose", "false"))
+
+	if verbose != "true" && verbose != "false" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "verbose must be true or false"})
+		return
+	}
+
+	response, return_code := ollama.ShowInfoModel(model, verbose)
 
 	if return_code != 200 {
 		c.JSON(http.StatusInternalServerError, gin.H{"ollama code": return_code})
 	} else {
-		c.JSON(http.StatusOK, gin.H{"ollama code": return_code})
+		c.JSON(http.StatusOK, gin.H{"ollama code": return_code, "response": response})
 	}
 
 }
